8b9608601d554043e734f12bf07a96ed95088e0a
Marcos <supermarcos@gmail.com>
2015-09-23 20:00:35 +1000

    my solution to assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix is a "function" that declares a bunch of properties for an "object", so to speak...
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    # set is the setter, the function that assigns y to x and clears inv on the upper level context
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    # get is the function that returns x, the matrix
+    get <- function() x
+    
+    # this setter assigns inverse to inv on the upper level context
+    setInverse <- function(inverse) inv <<- inverse
+    
+    # this getter just returns the variable inv
+    getInverse <- function() inv
+    
+    # exposed functions of x (set, get, setInverse and getInverse)
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve is the function that, given a matrix as an argument will return the inverse (wheter it is cached or not)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    # if there is something already in inv, then return that,
+    # if nothing in inv, we have to calculate the inverse of that matrix
+    if (is.null(inv)) {
+        # inverse the matrix calling the function setinverse of x
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+    } else {
+        message("the data is cached")
+    }
+    # this will return the cached data when is.null(inv) is not TRUE
+    return(inv)
 }
